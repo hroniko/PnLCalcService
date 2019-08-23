7bd7a88dad92518a478968a75663373f4705e519
hroniko <hroniko@bk.ru>
2019-08-23 11:22:14 +0300

    init commit

---
+++ src/test/java/com/hroniko/pnlservice/PnlServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hroniko.pnlservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PnlServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
