2e9e5c33b3f27e2f21821dd7521dae4d86580e17
hroniko <hroniko@bk.ru>
2019-08-27 15:45:13 +0300

    fix pom

--- src/main/java/com/hroniko/pnl/utils/PnLHelper.java
+++ src/main/java/com/hroniko/pnl/utils/PnLHelper.java
@@ -1,7 +1,5 @@
 package com.hroniko.pnl.utils;
 
-import com.netcracker.tbapi.services.oe.converter.OEConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
